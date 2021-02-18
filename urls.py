8cdc0841a55cb02c2c35ce04f0db023d152a81c6
jenifer-tech <72019417+jenifer-tech@users.noreply.github.com>
2021-02-18 11:24:43 +0530

    APP

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""proj URL Configuration
+"""prj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,16 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from app.views import BookList,BookDetail
-
-
-
+from django.contrib import admin
+from account.views import(
+	registration_view,
+	account_properties_view,
+	update_account_view,
+    ObtainAuthTokenView,
+)
+app_name = 'account'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/',obtain_auth_token),
-    path('api/books',BookList.as_view()),
-    path('api/books/<int:pk>',BookDetail.as_view()),
+	path('history/', account_properties_view, name="properties"),
+	path('history/update', update_account_view, name="update"),
+	path('login', ObtainAuthTokenView.as_view(), name="login"), 
+	path('register', registration_view, name="register"),
 ]
+
+
+
